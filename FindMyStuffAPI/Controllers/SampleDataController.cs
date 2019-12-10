6a0771d4e8291159edd4b5f1a09abcb0c5570036
Jose Gomez <gomezj6@aetna.com>
2019-12-10 15:07:01 -0500

    Pointed React App to API project using CORS

--- FindMyStuffAPI/Controllers/SampleDataController.cs
+++ FindMyStuffAPI/Controllers/SampleDataController.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FindMyStuff.Controllers
+namespace FindMyStuffAPI.Controllers
 {
+    [EnableCors]
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
